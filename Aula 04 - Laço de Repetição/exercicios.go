02b24f8c734780e244989595f0b3d48666d1cd8e
Leonardo <leonardoraiz@icloud.com>
2024-01-08 10:28:18 -0300

    Aula 04

--- Aula 04 - Laço de Repetição/exercicios.go
+++ Aula 04 - Laço de Repetição/exercicios.go
@@ -6,23 +6,33 @@ import (
 )
 
 func main() {
-	fmt.Println("--- Exercício 1 ---")
-	fmt.Println("")
-	Exercicios.Exercicio1()
+	i := 1
+	for i != 0 {
+		fmt.Println("Qual exercício você deseja visualizar?")
+		fmt.Println("O valor 0 sair do programa!")
+		fmt.Scanln(&i)
 
-	fmt.Println("")
-	fmt.Println("--- Exercício 2 ---")
-	fmt.Println("")
-	Exercicios.Exercicio2()
-
-	fmt.Println("")
-	fmt.Println("--- Exercício 3 ---")
-	fmt.Println("")
-	Exercicios.Exercicio3()
-
-	fmt.Println("")
-	fmt.Println("--- Exercício 4 ---")
-	fmt.Println("")
-	Exercicios.Exercicio4()
+		switch i {
+		case 1:
+			fmt.Println("--- Exercício 1 ---")
+			fmt.Println("")
+			Exercicios.Exercicio1()
+		case 2:
+			fmt.Println("")
+			fmt.Println("--- Exercício 2 ---")
+			fmt.Println("")
+			Exercicios.Exercicio2()
+		case 3:
+			fmt.Println("")
+			fmt.Println("--- Exercício 3 ---")
+			fmt.Println("")
+			Exercicios.Exercicio3()
+		case 4:
+			fmt.Println("")
+			fmt.Println("--- Exercício 4 ---")
+			fmt.Println("")
+			Exercicios.Exercicio4()
+		}
+	}
 
 }
